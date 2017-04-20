6ace90807b8e8d6d5bc53e49e833e3ec39a3b606
Eric Wan <eric.wan.97@gmail.com>
2017-04-20 00:50:26 -0400

    week 3

---
+++ Week 3/wan_e_072_lab3.m
@@ -0,0 +1,26 @@
+%% Eric Wna
+
+%% Question 1
+A = [1 2 -3; 2 5 -8; 3 8 -13]; % Settting values of A matrix
+B = [1; 4; 7]; % Setting values of B matrix
+augAB = [A, B] % Forming augmented matrix AB
+
+%% Question 2
+rref(augAB) % Finding reduced row echelon form
+
+%% Question 3
+% pivots: (1, 1), (2, 2)
+% basic variables: X1, X2
+% free variables: X3
+
+%% Question 4
+% X2 - 2X1
+% X3 - 3X1
+% 
+
+%% Quesiton 5
+X1 = 3;
+
+
+
+
